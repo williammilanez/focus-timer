1d94b4443e4363638214f16891a378966d05bffe
William Milanez <william_milanez@hotmail.com>
2024-10-30 21:29:36 -0300

    estruturando o countdown

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,8 +1,11 @@
 import state from './state.js'
+import * as timer from './timer.js'
 
 export function toggleRunning() {
     // state.isRunning = !state.isRunning -----> uma maneira de fazer
     state.isRunning = document.documentElement.classList.toggle('running')
+
+    timer.countdown()
 }
 
 export function reset() {
--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,10 +1,23 @@
 import state from './state.js'
 import * as el from './elements.js'
 
+export function countdown() {
+    if(!state.isRunning) {
+        return
+    }
+    
+    console.log('iniiiccciiiiooouuuu')
+    
+    setTimeout(() => countdown(), 1000)
+    // setTimeout é uma função que recebe um callback
+    // countdown é uma função que vai ser chamada de volta
+    // 1000 é uma unidade de tempo que representa milissegundo (ms) ou equivalente a 1 segundo (s)
+}
+
 export function updateDisplay(minutes, seconds) {
-    // nullish coalescing operator
     minutes = minutes ?? state.minutes
     seconds = seconds ?? state.seconds
+    // ?? se chama nullish coalescing operator
 
     el.minutes.textContent = String(minutes).padStart(2, "0")
     el.seconds.textContent = String(seconds).padStart(2, "0")
