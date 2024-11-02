f9713243ddcf6a09ba166e543fc038d6694e4b39
William Milanez <william_milanez@hotmail.com>
2024-11-02 18:25:06 -0300

    alterando conteúdo da tag - contenteditable

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,5 +1,6 @@
 import state from './state.js'
 import * as timer from './timer.js'
+import * as el from './elements.js'
 
 export function toggleRunning() {
     // state.isRunning = !state.isRunning -----> uma maneira de fazer
@@ -15,7 +16,7 @@ export function reset() {
 }
 
 export function set() {
-    
+    el.minutes.setAttribute('contenteditable', true)
 }
 
 export function toggleMusic() {
