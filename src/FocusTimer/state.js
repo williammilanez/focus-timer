817015dc1cc6bd1b8c57587302d8911a7e77ec3c
William Milanez <william_milanez@hotmail.com>
2024-11-03 21:37:59 -0300

    corrigindo o acúmulo de Timeout

--- src/FocusTimer/state.js
+++ src/FocusTimer/state.js
@@ -2,7 +2,8 @@ export default {
     minutes: 25,
     seconds: 0,
     isRunning: false,
-    isMute: true
+    isMute: true,
+    countdownId: null
 }
 
 
--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -4,6 +4,8 @@ import { reset } from './actions.js'
 import { kitchenTimer } from './sounds.js'
 
 export function countdown() {
+    clearTimeout(state.countdownId)
+
     if(!state.isRunning) {
         return
     }
@@ -26,7 +28,7 @@ export function countdown() {
 
     updateDisplay(minutes, seconds)
 
-    setTimeout(() => countdown(), 1000)
+    state.countdownId = setTimeout(() => countdown(), 1000)
     // setTimeout é uma função que recebe um callback
     // countdown é uma função que vai ser chamada de volta
     // 1000 é uma unidade de tempo que representa milissegundo (ms) ou equivalente a 1 segundo (s)
